59a887659a3cb1b0103e12f9f9d0d20f1fe64130
MatiasOrrego <orregomatias50@gmail.com>
2025-09-05 20:16:58 -0300

    merge con exito

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,13 @@ import docenteRoutes from './routes/docenteRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import roleRoutes from './routes/roleRoutes.js';
 import contratoRoutes from './routes/contratoRoutes.js';
+import authRouter from './routes/authr.routes.js';
+import docenteRouter from './routes/docente.routes.js';
+import userRouter from './routes/user.routes.js';
+import roleRouter from './routes/role.routes.js';
+import contratoRouter from './routes/contrato.routes.js';
+import personaDocRouster from './routes/personaDoc.routes.js';
+import personaRouter from './routes/persona.routes.js';
 
 const app = express();
 
@@ -18,7 +25,14 @@ app.use('/api/docente', docenteRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/roles', roleRoutes);
 app.use('/api/contratos', contratoRoutes);
+app.use('/api/auth', authRouter);
+app.use('/api/docente', docenteRouter);
+app.use('/api/users', userRouter);
+app.use('/api/roles', roleRouter);
 app.use('/uploads', express.static('uploads'));
+app.use('/api/contratos', contratoRouter);
+app.use('/api/persona-doc', personaDocRouster);
+app.use('/api/persona', personaRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
